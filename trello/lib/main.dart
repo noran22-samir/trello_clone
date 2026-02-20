8629dedb4c693d23809e7166ffc928b2990d568b
Noran Samir <nouran27as@gmail.com>
2026-02-20 17:12:05 +0200

    update splash loader and main routes

--- trello/lib/main.dart
+++ trello/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:trello/features/home/screens/home_screen.dart';
-import 'package:trello/features/login/screen/login_screen.dart';
-import 'package:trello/features/splash/screen/splash_screen.dart';
-import '../features/signUp/screen/sign_up_screen.dart';
-import './features/splash/screen/splash_screen.dart';
-import 'package:trello/features/on_boading/screens/onBoarding.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/home/screens/home_screen.dart';
+import 'features/login/screen/login_screen.dart';
+import 'features/splash/screen/splash_screen.dart';
+import 'features/splash/cubit/splash_cubit.dart';
+import 'features/signUp/screen/sign_up_screen.dart';
+import 'features/on_boading/screens/onBoarding.dart';
+
 void main() {
-  runApp(TrelloApp());
+  runApp(const TrelloApp());
 }
 
 class TrelloApp extends StatelessWidget {
@@ -14,20 +16,43 @@ class TrelloApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SplashCubit>(create: (_) => SplashCubit()..startSplash()),
+    /*
+    ================== TEAM NOTE ==================
 
-        '/on_boarding': (context) => OnboardingScreen(),
+      When you finish a Cubit for any Feature:
+    1️⃣ Add its BlocProvider here
+        Example:
+        BlocProvider<AuthCubit>(
+          create: (_) => AuthCubit(),
+        ),
 
+    2️⃣ Add the corresponding Screen Route below in `routes`
 
-        '/signUp': (context) => SignUpScreen(),
-        '/login': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(),
+    !!! Do NOT create BlocProviders inside Screens
+    !!! All Cubits should be Global from here
 
-      },
+    ===============================================
+    */
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+      /*
+        TEAM NOTE:
+        When you finish a new Screen:
+        - Add its Route here following the same pattern
+      */
+          '/': (context) => const SplashScreen(),
+          '/on_boarding': (context) => const OnboardingScreen(),
+          '/signUp': (context) => const SignUpScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/home': (context) => const HomeScreen(),
+        },
+      ),
     );
   }
 }
