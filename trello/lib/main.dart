2c750996e9dac673a789915afb765ed23e9eef10
Migoo0-ee <migo.ee0@gmail.com>
2026-02-20 23:14:35 +0200

    finish settings Ui

--- trello/lib/main.dart
+++ trello/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trello/features/home/screens/home_screen.dart';
 import 'package:trello/features/login/screen/login_screen.dart';
+import 'package:trello/features/settings/screens/settings_screen.dart';
 import 'package:trello/features/splash/screen/splash_screen.dart';
 import '../features/signUp/screen/sign_up_screen.dart';
 import 'package:trello/features/on_boading/screens/onBoarding.dart';
@@ -22,11 +23,10 @@ class TrelloApp extends StatelessWidget {
 
         '/on_boarding': (context) => OnboardingScreen(),
 
-
         '/signUp': (context) => SignUpScreen(),
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
-
+        '/settings': (context) => SettingsScreen(),
       },
     );
   }
