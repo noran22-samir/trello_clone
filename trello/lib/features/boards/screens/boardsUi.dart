46487d0d6e0488d980d537405df37e400cb38c8b
Shahd Adel <shahdadelsaed@gmail.com>
2026-03-04 04:49:58 +0200

    comment board-screen

--- trello/lib/features/boards/screens/boardsUi.dart
+++ trello/lib/features/boards/screens/boardsUi.dart
@@ -1,38 +1,38 @@
-import "package:flutter/material.dart";
+// import "package:flutter/material.dart";
 
-class boardsUi extends StatelessWidget {
-  const boardsUi({super.key});
+// class boardsUi extends StatelessWidget {
+//   const boardsUi({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: const Color.fromARGB(255, 17, 89, 147),
-          elevation: 3,
-          child: Icon(Icons.search_off_sharp, color: Colors.white),
-        ),
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {},
+//           backgroundColor: const Color.fromARGB(255, 17, 89, 147),
+//           elevation: 3,
+//           child: Icon(Icons.search_off_sharp, color: Colors.white),
+//         ),
+//         appBar: AppBar(
+//           leading: IconButton(
+//             icon: Icon(Icons.arrow_back),
+//             onPressed: () {
+//               Navigator.pop(context);
+//             },
+//           ),
 
-          title: Text("Boards Title"),
-          actions: [
-            Opacity(opacity: 0.4, child: Icon(Icons.sort)),
-            Opacity(opacity: 0.4, child: Icon(Icons.notifications)),
-            Opacity(opacity: 0.4, child: Icon(Icons.more_vert_rounded)),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: 
-        ),
-      ),
-    );
-  }
-}
+//           title: Text("Boards Title"),
+//           actions: [
+//             Opacity(opacity: 0.4, child: Icon(Icons.sort)),
+//             Opacity(opacity: 0.4, child: Icon(Icons.notifications)),
+//             Opacity(opacity: 0.4, child: Icon(Icons.more_vert_rounded)),
+//           ],
+//         ),
+//         body: SingleChildScrollView(
+//           child: 
+//         ),
+//       ),
+//     );
+//   }
+// }
